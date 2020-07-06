4bd3ba289a12c66a305c7cfcac4bcafa17f33008
Youri Westerman <youri.westerman@nedac.eu>
2020-07-06 12:35:17 +0200

    Running theme setup in the same fashion as the tests

--- run-behat.sh
+++ run-behat.sh
@@ -2,7 +2,7 @@
 set -eux
 
 if [[ $(cd tests/Application && git status | awk '{print $3}' | head -n1) == "master" ]]; then
-	vendor/bin/behat --tags=@theme_setup
+	vendor/bin/behat --tags=@theme_setup --strict -vvv --no-interaction || vendor/bin/behat --tags=@theme_setup --strict -vvv --no-interaction --rerun
 	(cd tests/Application && bin/console ca:cl)
 	vendor/bin/behat --tags="@theme" --strict -vvv --no-interaction || vendor/bin/behat --tags="@theme" --strict -vvv --no-interaction --rerun
 else
