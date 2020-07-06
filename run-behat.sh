62ccd3a6991b0f0c506689ee6020122399b92481
Youri Westerman <youri.westerman@nedac.eu>
2020-07-06 14:08:40 +0200

    Passing parameters along to all the commands

--- run-behat.sh
+++ run-behat.sh
@@ -2,9 +2,9 @@
 set -eux
 
 if [[ $(cd tests/Application && git status | awk '{print $3}' | head -n1) == "master" ]]; then
-	vendor/bin/behat --tags=@theme_setup --strict -vvv --no-interaction || vendor/bin/behat --tags=@theme_setup --strict -vvv --no-interaction --rerun
+	vendor/bin/behat --tags=@theme_setup --strict -vvv --no-interaction "$@"|| vendor/bin/behat --tags=@theme_setup --strict -vvv --no-interaction --rerun "$@"
 	(cd tests/Application && bin/console ca:cl)
-	vendor/bin/behat --tags="@theme" --strict -vvv --no-interaction || vendor/bin/behat --tags="@theme" --strict -vvv --no-interaction --rerun
+	vendor/bin/behat --tags="@theme" --strict -vvv --no-interaction "$@" || vendor/bin/behat --tags="@theme" --strict -vvv --no-interaction --rerun "$@"
 else
-	vendor/bin/behat --tags="~@theme&&~@theme_setup" --strict -vvv --no-interaction || vendor/bin/behat --tags="~@theme&&~@theme_setup" --strict -vvv --no-interaction --rerun
+	vendor/bin/behat --tags="~@theme&&~@theme_setup" --strict -vvv --no-interaction "$@" || vendor/bin/behat --tags="~@theme&&~@theme_setup" --strict -vvv --no-interaction --rerun "$@"
 fi
