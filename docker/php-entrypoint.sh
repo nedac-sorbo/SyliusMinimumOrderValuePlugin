38673dbd4135bfa4394c07ef86c41c3c843325fe
Youri Westerman <youri.westerman@nedac.eu>
2021-06-04 13:20:18 +0200

    DEV-211 Disabling filesystem access control lists in entrypoint

--- docker/php-entrypoint.sh
+++ docker/php-entrypoint.sh
@@ -6,7 +6,7 @@ if [ "${1#-}" != "$1" ]; then
   set -- php-fpm "$@"
 fi
 
-setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var public/media
-setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var public/media
+#setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var public/media
+#setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var public/media
 
 exec docker-php-entrypoint "$@"
