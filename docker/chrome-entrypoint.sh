e5ab38413f9366089ec15ccfb0e7324b52490b05
Youri Westerman <youri.westerman@nedac.eu>
2021-06-09 09:57:20 +0200

    DEV-211 Set environment to test by default

--- docker/chrome-entrypoint.sh
+++ docker/chrome-entrypoint.sh
@@ -5,5 +5,5 @@ set -e
 /opt/bin/start-xvfb.sh &
 sleep 20
 /opt/bin/start-vnc.sh &
-
-google-chrome-stable --enable-automation --disable-background-networking --no-default-browser-check --no-first-run --disable-popup-blocking --disable-default-apps --allow-insecure-localhost --disable-translate --disable-extensions --no-sandbox --enable-features=Metal --headless --remote-debugging-port=9222 --window-size=2880,1800 --proxy-server='direct://' --proxy-bypass-list='*' http://127.0.0.1
+sleep 20
+google-chrome-stable --enable-automation --disable-background-networking --no-default-browser-check --no-first-run --disable-popup-blocking --disable-default-apps --allow-insecure-localhost --disable-translate --disable-extensions --no-sandbox --enable-features=Metal --remote-debugging-port=9222 --window-size=1920,1080 --proxy-server='direct://' --proxy-bypass-list='*' http://127.0.0.1
